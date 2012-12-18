add31c3ec9a69118ad56b30d80ff9afd4c6aeb4b
willaser <willaser@gmail.com>
2012-12-18 01:23:38 +0100

    #1 Evento para eliminar las reservas libres pasadas

--- modules/lab/events.php
+++ modules/lab/events.php
@@ -16,7 +16,32 @@ class Events_Lab
         $this->ci =& get_instance();
 
         // register the public_controller event when this file is autoloaded
+        Events::register('public_controller', array($this, 'delete_unreserved_events'));
         Events::register('streams_post_insert_entry', array($this, 'create_booking'));
+    }
+
+    // this will be triggered by the Events::trigger('public_controller') code in Public_Controller.php
+    public function delete_unreserved_events()
+    {
+        $date = new DateTime();
+        $date->add(new DateInterval('PT3H'));
+
+        $this->ci->db->where('date_start <', $date->format('Y-m-d H:m:s'));
+        $result = $this->ci->db->get('calendar');
+
+        foreach ($result->result() as $entry) {
+
+            $this->ci->db->where('id', $entry->id);
+            if(!$this->ci->db->delete('calendar'))
+                log_message('error', 'Delete entry in calendar');
+
+            $this->ci->db->where('id', $entry->stream_entry_id);
+            if(!$this->ci->db->delete('c_i_events_list'))
+                log_message('error', 'Delete entry in c_i_events_list');
+        }
+
+        // you can load a model or etc here if you like using $this->ci->load();
+        return 'The streams_post_insert_entry has ran';
     }
 
     // this will be triggered by the Events::trigger('public_controller') code in Public_Controller.php
@@ -32,7 +57,7 @@ class Events_Lab
 
             $this->ci->db->where('date_start', $post['insert_data']['booking_date']);
             if(!$this->ci->db->delete('calendar'))
-                log_message('error', 'Delete entry in c_i_events_list');
+                log_message('error', 'Delete entry in calendar');
         }
 
         // you can load a model or etc here if you like using $this->ci->load();
